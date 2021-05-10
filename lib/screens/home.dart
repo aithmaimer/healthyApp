daafefee758fe54c57225d76fda383285f524762
aitHmaimer <aithmaimerabderrahim@gmail.com>
2021-05-10 14:56:57 +0000

    verification email terminer

---
+++ lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "my list Viwe",
+        home: Scaffold(
+            body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.access_alarm),
+              title: Text("My alarm"),
+              subtitle: Text("the body of the alarm list"),
+              trailing: Icon(Icons.accessibility),
+              onTap: () {
+                // onclick(context);
+                debugPrint("heloo world");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.adb),
+              title: Text("My android"),
+              subtitle: Text("the body of the android list"),
+              trailing: Icon(Icons.image),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle_rounded),
+              title: Text("My account"),
+              trailing: Icon(Icons.account_circle),
+            ),
+          ],
+        )));
+  }
+}
